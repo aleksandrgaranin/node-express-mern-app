06c8636bf93407caaab96f431508ae6f6af3234b
aleksandGG <aleksandrgaranin89@gmail.com>
2021-03-11 14:49:00 -0800

    validation added

--- controllers/places-controller.js
+++ controllers/places-controller.js
@@ -1,5 +1,6 @@
 const HttpError = require('../models/http-error')
 const uuid = require('uuid/v4')
+const { validationResult } = require('express-validator')
 
 
 let DUMMY_PLACES = [
@@ -47,6 +48,13 @@ const getPlacesByUserId = (req, res, next) => {
 
 const createPlace = (req, res, next) => {
   const { title, description, coordinates, address, creator } = req.body
+
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    console.log(errors)
+    throw new HttpError("Invalid inputs passed, please check your data.", 422)
+  }
+
   const createdPlace = {
     id: uuid(),
     title,
@@ -62,6 +70,12 @@ const createPlace = (req, res, next) => {
 }
 
 const updatePlace = (req, res, next) => {
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    console.log(errors)
+    throw new HttpError("Invalid inputs passed, please check your data.", 422)
+  }
+
   const { title, description } = req.body
   const placeId = req.params.pid
 
@@ -79,8 +93,11 @@ const updatePlace = (req, res, next) => {
 
 const deletePlace = (req, res, next) => {
   const placeId = req.params.pid
+  if (!DUMMY_PLACES.find(p => p.id === placeId)) {
+    throw new HttpError("Could not find a place for that id", 404)
+  }
   DUMMY_PLACES = DUMMY_PLACES.filter(p => p.id !== placeId)
-  res.status(200).json({message: 'Deleted place.'})
+  res.status(200).json({ message: 'Deleted place.' })
 }
 
 exports.getPlaceById = getPlaceById
--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -1,5 +1,6 @@
 const HttpError = require('../models/http-error')
 const uuid = require('uuid/v4')
+const { validationResult } = require('express-validator')
 
 const DUMMY_USERS = [
   {
@@ -31,6 +32,12 @@ const getUsers = (req, res, next) => {
 
 const singupUser = (req, res, next) => {
   const { userName, email, password } = req.body
+
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    console.log(errors)
+    throw new HttpError("Invalid inputs passed, please check your data.", 422)
+  }
 
   const hasUser = DUMMY_USERS.find(u => u.email === email)
   if (hasUser) {
