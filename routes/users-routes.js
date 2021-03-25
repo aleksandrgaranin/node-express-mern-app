206f4acc8f316f084ba1753ef52e68b7c70b44d7
aleksandGG <aleksandrgaranin89@gmail.com>
2021-03-25 04:52:38 -0700

    Added multer package + configured to store file in local file sustem

--- routes/users-routes.js
+++ routes/users-routes.js
@@ -2,17 +2,19 @@ const express = require('express');
 const { check } = require('express-validator')
 
 const usersControllers = require('../controllers/users-controllers')
+const fileUpload = require('../middleware/file-upload')
 
 const router = express.Router();
 
 router.get('/', usersControllers.getUsers)
-router.post('/signup', 
-[
-  check("userName").not().isEmpty(),
-  check("email").normalizeEmail().isEmail(),
-  check("password").isLength({min: 6})
-],
-usersControllers.singupUser)
+router.post('/signup',
+  fileUpload.single('image'),
+  [
+    check("userName").not().isEmpty(),
+    check("email").normalizeEmail().isEmail(),
+    check("password").isLength({ min: 6 })
+  ],
+  usersControllers.singupUser)
 router.post('/login', usersControllers.loginUser)
 
 
